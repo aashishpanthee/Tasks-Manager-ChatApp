fd7255956a64f2ea476c1de93290d2f73d69c0d6
Aashish Panthee <aashishpanthee060@gmail.com>
2024-01-18 17:58:11 +0545

    Add background to LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,9 +3,11 @@ import Login from "../components/Login";
 
 const LoginPage = () => {
   return (
-    <div className='bg-blue-600 h-screen flex items-center justify-center'>
+    <div className='flex items-center justify-center h-screen p-6'>
       {/* Login */}
       <Login />
+      <div className='absolute top-0 w-full h-screen -z-10 opacity-70 bg-gradient-to-r from-myBlue to-myPink' />
+      <div className='absolute top-0 w-full h-screen bg-no-repeat bg-cover bg-pattern -z-20' />
     </div>
   );
 };
