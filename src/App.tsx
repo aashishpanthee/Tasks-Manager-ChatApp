73d71d4277192a70701b2bf00509a540cd06cdb8
Aashish Panthee <aashishpanthee060@gmail.com>
2024-01-18 22:17:18 +0545

    Add routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import ListPage from "./pages/ListPage";
+import ChatPage from "./pages/ChatPage";
+import ProfilePage from "./pages/ProfilePage";
+import Layout from "./pages/Layout";
 
 function App() {
   return (
-    <div className=''>
-      <LoginPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth' element={<LoginPage />} />
+        <Route path='/dashboard' element={<Layout />}>
+          <Route index element={<ListPage />} />
+          <Route path='chat' element={<ChatPage />} />
+          <Route path='profile' element={<ProfilePage />} />
+        </Route>
+        <Route path='*' element={<Navigate to='/dashboard' />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
