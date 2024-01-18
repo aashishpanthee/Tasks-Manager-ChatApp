29135f55004f4b05a3ab8b91d10606b94f6a605c
Aashish Panthee <aashishpanthee060@gmail.com>
2024-01-18 18:34:54 +0545

    Add useState for conditional rendering

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./Input";
 import Button from "./Button";
 
 const Login = () => {
+  const [login, setLogin] = useState(true);
   return (
     <div className='w-full md:w-[450px]'>
       <h1 className='mb-10 text-4xl font-bold text-center text-white md:text-6xl'>
-        Login
+        {login ? "Login" : "Register"}
       </h1>
       <div className='bg-white w-full p-6 min-h-[150px] flex flex-col gap-3 rounded-xl'>
         <Input name='email' type='email' />
         <Input name='password' type='password' />
-        <Input name='confirm password' type='password' />
-        <Button text='Login' className='' loading/>
-        <Button text='Register' className='' secondary />
+        {!login && <Input name='confirm password' type='password' />}
+        {login ? (
+          <>
+            <Button text='Login' className='' loading />
+            <Button
+              text='Register'
+              className=''
+              secondary
+              onClick={() => setLogin(false)}
+            />
+          </>
+        ) : (
+          <>
+            <Button text='Register' className='' loading />
+            <Button
+              text='Login'
+              className=''
+              secondary
+              onClick={() => setLogin(true)}
+            />
+          </>
+        )}
       </div>
     </div>
   );
