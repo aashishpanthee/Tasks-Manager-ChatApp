30efb875b816096c98a80cbdf8eeec2d9ab40fc1
Aashish Panthee <aashishpanthee060@gmail.com>
2024-01-22 23:05:42 +0545

    Add catchErr.ts inside src/utils -this function is used for catching error and toasting it in UI. -Added react-toastify package for toast notification.

---
+++ src/utils/catchErr.ts
@@ -0,0 +1,20 @@
+import { toastError } from "./toast";
+
+const catchErr = (err: { code?: string }) => {
+  const { code } = err;
+  if (code === "auth/invalid-email") toastError("Enter a valid email");
+  else if (code === "auth/weak-password")
+    toastError("Password must be at least 6 characters logn");
+  else if (code === "auth/user-not-found") toastError("User not found");
+  else if (code === "auth/invalid-password") toastError("Invalid password");
+  else if (code === "auth/email-already-exists")
+    toastError("Email already exists");
+  else if (code === "auth/email-already-in-use")
+    toastError("Email already exists");
+  else if (code === "auth/invalid-id-token") toastError("Invalid token");
+  else if (code === "auth/id-token-expired")
+    toastError("Token expired !! Login again");
+  else toastError("Something went wrong");
+  console.log(err);
+};
+export default catchErr;
