41f55640733aa433a6dd137f4b49ab6a97625a19
Aashish Panthee <aashishpanthee060@gmail.com>
2024-01-23 17:17:48 +0545

    Add defaultUser to initialState & set user to localStorage after successfull auth

--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -1,13 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { userType } from "../Types";
 
-const initialState = {};
+export const defaultUser: userType = {
+  id: "",
+  username: "",
+  email: "",
+  isOnline: false,
+  img: "",
+  bio: "",
+  creationTime: "",
+  lastSeen: "",
+};
+
+const initialState = {
+  currentUser: defaultUser,
+};
 
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
     setUser: (state, action) => {
+      const user = action.payload;
+      // set user in local storage
+      localStorage.setItem("user", JSON.stringify(user));
       // set logged in user
+      state.currentUser = user;
     },
     setUsers: (state, action) => {
       // set all users
