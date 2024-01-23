a36838e9f31578096b11c60223017bcd0176d06d
Aashish Panthee <aashishpanthee060@gmail.com>
2024-01-23 17:15:33 +0545

    Modify toast.ts to pass setLoading prop

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -1,14 +1,27 @@
 import { toast } from "react-toastify";
+import { setLoadingType } from "../Types";
 
-export const toastError = (mesg: string) => {
+export const toastError = (mesg: string, setLoading?: setLoadingType) => {
   toast.error(mesg);
+  if (setLoading) {
+    setLoading(false);
+  }
 };
-export const toastSuccess = (mesg: string) => {
+export const toastSuccess = (mesg: string, setLoading?: setLoadingType) => {
   toast.success(mesg);
+  if (setLoading) {
+    setLoading(false);
+  }
 };
-export const toastWarning = (mesg: string) => {
+export const toastWarning = (mesg: string, setLoading?: setLoadingType) => {
   toast.warn(mesg);
+  if (setLoading) {
+    setLoading(false);
+  }
 };
-export const toastInfo = (mesg: string) => {
+export const toastInfo = (mesg: string, setLoading?: setLoadingType) => {
   toast.info(mesg);
+  if (setLoading) {
+    setLoading(false);
+  }
 };
