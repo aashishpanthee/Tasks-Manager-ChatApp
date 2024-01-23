69074ebeff11916146f352098e4dd65d73eb5a3c
Aashish Panthee <aashishpanthee060@gmail.com>
2024-01-23 17:27:28 +0545

    Add functions for adding userinfo,updating userinfo to database, -Also added functions for getting userinfo from database, and deleting userinfo from database -used avatar generating function while creating user into database -added function called getStorageUser to get userinfo from localstorage and update to database.

--- src/Backend/Queries.ts
+++ src/Backend/Queries.ts
@@ -3,24 +3,53 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "./Firebase";
+import {
+  doc,
+  serverTimestamp,
+  setDoc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { toastError } from "../utils/toast";
+import { db } from "./Firebase";
 import catchErr from "../utils/catchErr";
-import { authDataType, setLoadingType } from "../Types";
+import { authDataType, setLoadingType, userType } from "../Types";
 import { NavigateFunction } from "react-router-dom";
+import { defaultUser, setUser } from "../redux/userSlice";
+import { AppDispatch } from "../redux/store";
+import { convertTime } from "../utils/convertTime";
+import { avatargenerator } from "../utils/avatargenerator";
+
+// collection name
+enum Collections {
+  USERSCOLL = "users",
+  TASKSCOLL = "tasks",
+  TASKLISTCOLL = "tasklist",
+  CHATSCOLL = "chats",
+  MESSAGESCOLL = "messages",
+}
 
 export const BE_signUp = (
   data: authDataType,
   setLoading: setLoadingType,
   reset: () => void,
-  routeTo: NavigateFunction
+  routeTo: NavigateFunction,
+  dispatch: AppDispatch
 ) => {
   const { email, password, confirmPassword } = data;
   setLoading(true);
   if (email && password) {
     if (password === confirmPassword) {
       createUserWithEmailAndPassword(auth, email, password)
-        .then(({ user }) => {
-          console.log(user);
+        .then(async ({ user }) => {
+          const avatar = avatargenerator(user.email?.split("@")[0] || "");
+          const userInfo = await addUserToCollection(
+            user.uid,
+            user.email || "",
+            user.email?.split("@")[0] || "",
+            avatar
+          );
+          dispatch(setUser(userInfo));
           setLoading(false);
           reset();
           routeTo("/dashboard");
@@ -30,12 +59,10 @@ export const BE_signUp = (
           setLoading(false);
         });
     } else {
-      toastError("Passwords must match");
-      setLoading(false);
+      toastError("Passwords must match", setLoading);
     }
   } else {
-    toastError("Field's can't be empty");
-    setLoading(false);
+    toastError("Field's can't be empty", setLoading);
   }
 };
 
@@ -43,13 +70,19 @@ export const BE_signIn = (
   data: authDataType,
   setLoading: setLoadingType,
   reset: () => void,
-  routeTo: NavigateFunction
+  routeTo: NavigateFunction,
+  dispatch: AppDispatch
 ) => {
   const { email, password } = data;
   setLoading(true);
   signInWithEmailAndPassword(auth, email, password)
-    .then(({ user }) => {
-      console.log(user);
+    .then(async ({ user }) => {
+      //update user info
+      await updateUserInfo({ id: user.uid, isOnline: true });
+      // get user info
+      const userInfo = await getUserInfo(user.uid);
+      // set user in store
+      dispatch(setUser(userInfo));
       setLoading(false);
       reset();
       routeTo("/dashboard");
@@ -58,4 +91,79 @@ export const BE_signIn = (
       catchErr(err);
       setLoading(false);
     });
+};
+
+export const addUserToCollection = async (
+  id: string,
+  email: string,
+  username: string,
+  img: string
+) => {
+  await setDoc(doc(db, Collections.USERSCOLL, id), {
+    isOnline: true,
+    img,
+    username,
+    email,
+    creationTime: serverTimestamp(),
+    lastSeen: serverTimestamp(),
+    bio: `Hey there! My name is ${username}`,
+  });
+  return getUserInfo(id);
+};
+
+const getUserInfo = async (id: string): Promise<userType> => {
+  const userRef = doc(db, Collections.USERSCOLL, id);
+  const user = await getDoc(userRef);
+  if (user.exists()) {
+    const { img, isOnline, username, bio, email, creationTime, lastSeen } =
+      user.data();
+    return {
+      id: user.id,
+      img,
+      isOnline,
+      email,
+      username,
+      bio,
+      creationTime: creationTime
+        ? convertTime(creationTime.toDate())
+        : "no-date-yet",
+      lastSeen: lastSeen ? convertTime(lastSeen.toDate()) : "no-date-yet",
+    };
+  } else {
+    toastError("getUserInfo:User not found");
+    return defaultUser;
+  }
+};
+
+export const updateUserInfo = async ({
+  id,
+  username,
+  img,
+  isOffline,
+  isOnline,
+}: {
+  id?: string;
+  username?: string;
+  img?: string;
+  isOffline?: boolean;
+  isOnline?: boolean;
+}) => {
+  if (!id) {
+    id = getStorageUser().id;
+  }
+  if (id) {
+    await updateDoc(doc(db, Collections.USERSCOLL, id), {
+      ...(username && { username }),
+      ...(img && { img }),
+      ...(isOnline && { isOnline }),
+      ...(isOffline && { isOnline: false }),
+      lastSeen: serverTimestamp(),
+    });
+  }
+};
+
+const getStorageUser = () => {
+  const usr = localStorage.getItem("user");
+  if (usr) return JSON.parse(usr);
+  else return null;
 };
