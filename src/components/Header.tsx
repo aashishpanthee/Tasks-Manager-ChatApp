a2764c13d0de641f4163becd53902b37e2a2cb31
Aashish Panthee <aashishpanthee060@gmail.com>
2024-01-24 20:50:52 +0545

    Conditional Render of Icons acc. to url/routes in Header.jsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,8 +17,20 @@ const Header = (props: Props) => {
   const dispatch = useDispatch();
   const routeTo = useNavigate();
   const { currentUser } = useSelector((state: RootState) => state.user);
+
+  const handleGoToPage = (page: string) => {
+    routeTo("/dashboard/" + page);
+    setCurrentPage(page);
+  };
+
   const handleSignout = () => {
     BE_signOut(dispatch, routeTo, setLogoutLoading);
+  };
+  const setCurrentPage = (page: string) => {
+    localStorage.setItem("user-page", page);
+  };
+  const getCurrentPage = () => {
+    return localStorage.getItem("user-page");
   };
   return (
     <div className='flex flex-wrap items-center justify-between gap-5 px-5 py-5 text-white bg-gradient-to-r from-myBlue to-myPink md:py-2 drop-shadow-md sm:flex-row'>
@@ -28,14 +40,38 @@ const Header = (props: Props) => {
         className='w-[70px] drop-shadow-md cursor-pointer'
       />
       <div className='flex flex-row-reverse items-center justify-center gap-5 md:flex-row flexwrap'>
-        <AddListBoard />
-        <Icon IconName={BsChatFill} ping={true} />
-        <Icon IconName={FiList} />
+        {getCurrentPage() === "chat" ? (
+          <Icon IconName={FiList} onClick={() => handleGoToPage("")} />
+        ) : getCurrentPage() === "profile" ? (
+          <>
+            <Icon IconName={FiList} onClick={() => handleGoToPage("")} />
+            <Icon
+              IconName={BsChatFill}
+              ping={true}
+              onClick={() => handleGoToPage("chat")}
+            />
+          </>
+        ) : (
+          <>
+            <AddListBoard />
+            <Icon
+              IconName={BsChatFill}
+              ping={true}
+              onClick={() => handleGoToPage("chat")}
+            />
+          </>
+        )}
+
         <div className='relative group'>
           <UserHeaderProfile user={currentUser} />
           <div className='absolute hidden w-full pt-5 group-hover:block min-w-max '>
             <ul className='w-full pt-1 overflow-hidden text-gray-700 bg-white rounded-md shadow-md '>
-              <div className='block px-4 py-2 hover:bg-gray-200'>Profile</div>
+              <div
+                className='block px-4 py-2 cursor-pointer hover:bg-gray-200'
+                onClick={() => handleGoToPage("profile")}
+              >
+                Profile
+              </div>
               <div
                 onClick={() => !logoutLoading && handleSignout()}
                 className={`flex flex-row items-center gap-4 px-4 py-2 cursor-pointer hover:bg-gray-200 ${
